81e0ea052ebc737c5528ac229dc2b17f34a14fc2
Jhonah <buryakone@gmail.com>
2021-11-25 00:05:06 +0700

    перенесли в аппу входные данные и обертку компонентов с данными

--- App.js
+++ App.js
@@ -2,11 +2,53 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import Hoc from './src/hoc';
 import { StockList } from './src/screens/views/stock-list.view';
+import { UserList } from './src/screens/views/user-list.view';
+
+const StocksData = [
+  {
+      id: 1,
+      name: 'TCS'
+  },
+  {
+      id: 2,
+      name: 'Infosys'
+  },
+  {
+      id: 3,
+      name: 'Reliance'
+  }
+];
+
+const UsersData = [
+  {
+      id: 1,
+      name: 'Krunal'
+  },
+  {
+      id: 2,
+      name: 'Ankit'
+  },
+  {
+      id: 3,
+      name: 'Rushabh'
+  }
+];
+
+const Stocks = Hoc(
+  StockList,
+  StocksData
+);
+
+const Users = Hoc(
+  UserList,
+  UsersData
+)
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <StockList></StockList>
+      <Users></Users>
+      <Stocks></Stocks>
     </View>
   );
 }
