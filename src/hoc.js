c34abcbc3eb913d2a4c5dc55ec9af4a4521a9a66
Jhonah <buryakone@gmail.com>
2021-11-25 00:06:47 +0700

    описали конструктор и доб данные возвращаемому компоненту

--- src/hoc.js
+++ src/hoc.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 
-export default function Hoc(HocComponent) {
+export default function Hoc(HocComponent, data) {
     return class extends Component {
         constructor(props) {
             super(props);
+            this.state = {
+                data: data
+            };
         }
 
         render(){
             return (
-                <View>
-                    <HocComponent></HocComponent>
-                </View>
+                <HocComponent data={this.state.data} {...this.props} />
             );
         }
     }
